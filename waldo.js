6434806886f94b69bc9350be2e25f22ebb666dde
DevYves <yfreypons@gmail.com>
2017-11-02 19:11:00 +0000

    For Each added take away for loop

--- waldo.js
+++ waldo.js
@@ -1,10 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+ arr.forEach(function(i, index){
+   if (i === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+ });
 }
 
 function actionWhenFound(i) {
